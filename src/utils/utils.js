f2704f2fc49e661202dcb97d4f40307776aab0d7
zavx0z <metaversebdfl@gmail.com>
2023-12-21 14:56:31 +0300

    refactor(utils): utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,16 +1,14 @@
-/** Выравнивание массива.
- * @template T
- * @param {Array<T | T[]>} array - Массив с элементами или с вложенными массивами.
- * @returns {T[]} - Одноуровневый массив.
+/** Array flattening.
+ * @template T - The type of the array elements.
+ * @param {Array<T | T[]>} array - The array to flatten.
+ * @returns {T[]} - The flattened array.
  */
 export const flatten = (array) => Array.prototype.concat.apply([], array)
 
-/**
- * Generates a UUID v4 string.
- */
-export function uuidv4() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-    //@ts-ignore
-    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+/** Generates a UUID v4 string. 
+ * @returns {string} - The UUID v4 string.
+*/
+export const uuidv4 = () =>
+  "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (parseInt(c) ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c) / 4)))).toString(16)
   )
-}
